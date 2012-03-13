c8299a133eb464d0db1263dde3554831c683bd33
Clint Savage <herlo1@gmail.com>
2012-03-13 10:55:37 -0600

    initial commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+from distutils.core import setup
+
+setup(name='anaconda-installclass-goose', version='0.1',
+      description='Python addin class for anaconda',
+      author='Clint Savage', author_email='herlo@gooseproject.org',
+      url='https://github.com/gooseproject/anaconda-installclass-goose',
+      packages=['pyanaconda.installclasses'])
